0d55a232cf7c0fdb2b5aebef4510bcb6ba094a6a
Rushang Bagada <rushang697@gmail.com>
2025-01-25 12:40:57 +0530

    password generator

--- purv bhai/react practice/03passwordgenerator/src/App.jsx
+++ purv bhai/react practice/03passwordgenerator/src/App.jsx
@@ -29,19 +29,19 @@ function App() {
   }, [password])
 
   return (
-    <div className="bg-[#A1d1b1] h-[100vh] flex justify-center items-center ">
-      <div className=" border-3 hover:border-5 shadow-2xl flex bg-[#1C2529] p-10 flex-col justify-center w-fit items-center h-fit rounded-2xl">
-        <h1 className="text-white text-3xl pb-7  ">Password Generator</h1>
+    <div className="bg-slate-900 h-[100vh] flex justify-center items-center ">
+      <div className="flex bg-slate-600 p-10  flex-col justify-center w-fit items-center h-fit rounded-2xl">
+        <h1 className="text-white text-2xl pb-7">Password generator</h1>
       
         <div className="mb-4 w-full flex justify-between">
-          <input className="border-2 shadow-2xl cursor-pointer mr-4 w-[300px] bg-white py-2 px-4 rounded-lg" readOnly={true} type="text" value={password} />
-          <button onClick={copy} className= "border-2 cursor-pointer hover:bg-[#A1d1b1] hover:text-black text-white rounded-lg px-4 py-2 bg-slate-700">copy</button>
+          <input className="w-[300px] bg-white py-2 px-4 rounded-lg" readOnly={true} type="text" value={password} />
+          <button onClick={copy} className="text-white rounded-lg px-4 py-2 bg-slate-700">copy</button>
         </div>
         <div className="flex  justify-around items-center">
           <input
-            className=" bg-white  rounded-lg" type="range" min="6" max="100"
+            className="bg-white" type="range" min="6" max="100"
            onChange={(e) => setlength(e.target.value)}/>
-          <label  className="text-white mx-1 px-1  ">{length}</label>
+          <label  className="text-white px-3 ">{length}</label>
           <input
             defaultChecked={charaloud}
             onChange={() => setcharaloud((charaloud)=>!charaloud) }
@@ -53,7 +53,7 @@ function App() {
             type="checkbox" name="" id="" />
           <label className="text-white px-3" htmlFor=""> numbers</label>
         </div>
-        <button onClick={passwordgenerator} className="border-2 cursor-pointer hover:bg-[#A1d1b1] hover:text-black text-white rounded-lg px-4 py-4 mt-4 mb-0 bg-slate-700">Generate</button>
+        <button onClick={passwordgenerator} className="text-white rounded-lg px-4 py-4 mt-4 mb-0 bg-slate-700">Generate</button>
       </div>
     </div>
   )
