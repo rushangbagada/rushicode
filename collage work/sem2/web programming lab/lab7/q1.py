66de0cbe4ba158f08bb867e4980414b6996b4fac
Rushang Bagada <rushang697@gmail.com>
2025-02-24 23:29:04 +0530

    new

---
+++ collage work/sem2/web programming lab/lab7/q1.py
@@ -0,0 +1,49 @@
+
+age = 25  
+print("Integer:", age)  
+
+height = 5.9  
+print("Float:", height)  
+
+
+name = "Alice"  
+print("String:", name)  
+
+fruits = ["apple", "banana", "cherry"]  
+print("List:", fruits)  
+
+
+coordinates = (10.0, 20.0)  
+print("Tuple:", coordinates)  
+
+ 
+person = {  
+    "name": "Bob",  
+    "age": 30,  
+    "city": "New York"  
+}  
+print("Dictionary:", person)  
+
+unique_numbers = {1, 2, 3, 2, 1}  
+print("Set:", unique_numbers)  
+
+is_student = True  
+print("Boolean:", is_student)  
+
+  
+nothing = None  
+print("NoneType:", nothing)  
+
+
+
+print(f"Type of age: {type(age)}")  
+print(f"Type of height: {type(height)}")  
+print(f"Type of name: {type(name)}")  
+print(f"Type of fruits: {type(fruits)}")  
+print(f"Type of coordinates: {type(coordinates)}")  
+print(f"Type of person: {type(person)}")  
+print(f"Type of unique_numbers: {type(unique_numbers)}")  
+print(f"Type of is_student: {type(is_student)}")  
+print(f"Type of nothing: {type(nothing)}")  
+
+
---
+++ collage work/sem2/web programming lab/lab7/q7.py
@@ -0,0 +1,8 @@
+# Write a python program to print a number is positive/negative using if-else. 
+
+num = int(input("Enter a number: "))
+
+if num > 0: 
+    print("positive number.")
+else:
+    print("negative number.")
